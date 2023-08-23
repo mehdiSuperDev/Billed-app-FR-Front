7c0c845331f529c8f29436d2679558b4e8da9af8
Mehdi Raheemun <mraheemu@student.42.fr>
2023-08-23 12:23:42 +0200

    wip

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -10,6 +10,13 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
 
+import Bills from "../containers/Bills.js";
+import store from "../__mocks__/store";
+jest.mock("../app/format.js", () => ({
+  formatDate: jest.fn((date) => `formatted-${date}`),
+  formatStatus: jest.fn((status) => `formatted-${status}`),
+}));
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
@@ -42,6 +49,79 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+  });
+  describe("when i click on NewBill button", () => {
+    test("Then, if i click on NewBill button, i will navigate to New Bill", () => {
+      // Mock des dépendances
+      const onNavigate = jest.fn();
+
+      // Initialisation du composant testé
+      const bills = new Bills({
+        document: document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage,
+      });
+
+      // Appel de la méthode testée
+      bills.handleClickNewBill();
+
+      // Vérification
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["NewBill"]);
+    });
+  });
+
+  //Test d'intégration GET
+  describe("Get Bills", () => {
+    let bills;
+    let mockedStore;
+
+    beforeEach(() => {
+      mockedStore = store;
+      bills = new Bills({
+        document: document,
+        onNavigate: jest.fn(),
+        store: mockedStore,
+        localStorage: jest.fn(),
+      });
+    });
+
+    describe("getBills()", () => {
+      it("does nothing if store is not defined", async () => {
+        bills.store = null;
+        expect(await bills.getBills()).toBeUndefined();
+      });
+
+      it("calls list() on this.store.bills()", async () => {
+        const spy = jest.spyOn(mockedStore.bills(), "list");
+        await bills.getBills();
+        expect(spy).toHaveBeenCalled();
+      });
+
+      it("formats the dates and statuses correctly", async () => {
+        const snapshot = await store.bills().list();
+        const result = await bills.getBills();
+
+        result.forEach((bill, index) => {
+          const originalDate = snapshot[index].date;
+          const originalStatus = snapshot[index].status;
+
+          expect(bill.date).toBe(`formatted-${originalDate}`);
+          expect(bill.status).toBe(`formatted-${originalStatus}`);
+        });
+      });
+
+      it("returns the original date and formatted status when date formatting fails", async () => {
+        const originalDate = "2001-01-01";
+        require("../app/format.js").formatDate.mockImplementationOnce(() => {
+          throw new Error("Formatting failed");
+        });
+        const result = await bills.getBills();
+        const failedBill = result.find((bill) => bill.date === originalDate);
+        expect(failedBill.date).toBe(originalDate);
+        expect(failedBill.status).toBe(`formatted-${failedBill.status}`);
+      });
     });
   });
 });
