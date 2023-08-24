714c8c4ef029a28e0a41d64cf23440afa0f08cf0
Mehdi Raheemun <mraheemu@student.42.fr>
2023-08-24 14:17:57 +0200

    test newBill --> 404/500

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,6 +5,10 @@ import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { fireEvent } from "@testing-library/dom";
 import "@testing-library/jest-dom";
 import mockStore from "../__mocks__/store";
+import router from "../app/Router";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
@@ -98,6 +102,55 @@ describe("Given I am connected as an employee", () => {
         expect(window.alert).not.toHaveBeenCalled();
         expect(newBill.fileUrl).toBe("https://localhost:3456/images/test.jpg");
       });
+    });
+  });
+
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills");
+
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "test@employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.appendChild(root);
+      router();
+    });
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list: () => {
+            return Promise.reject(new Error("Erreur 404"));
+          },
+        };
+      });
+      window.onNavigate(ROUTES_PATH.Bills);
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 404/);
+      expect(message).toBeTruthy();
+    });
+
+    test("fetches messages from an API and fails with 500 message error", async () => {
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list: () => {
+            return Promise.reject(new Error("Erreur 500"));
+          },
+        };
+      });
+
+      window.onNavigate(ROUTES_PATH.Bills);
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 500/);
+      expect(message).toBeTruthy();
     });
   });
 });
