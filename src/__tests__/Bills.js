99369025c19bfececbddb42b61e4716c4622c5c5
Mehdi Raheemun <mraheemu@student.42.fr>
2023-08-31 11:53:10 +0200

    wip

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -72,7 +72,6 @@ describe("Given I am connected as an employee", () => {
     });
   });
 
-  //Test d'intégration GET
   describe("getBills()", () => {
     let bills;
     let mockedStore;
@@ -158,6 +157,72 @@ describe("Given I am connected as an employee", () => {
 
       // Réinitialiser les mocks
       jest.restoreAllMocks();
+    });
+  });
+
+  //Test d'intégration GET
+  describe("Given I am a user connected as Employee", () => {
+    describe("When I navigate to bills", () => {
+      test("fetches bills from mock API GET", async () => {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ type: "Employee", email: "a@a" })
+        );
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.append(root);
+        router();
+        window.onNavigate(ROUTES_PATH.Bills);
+        const contentPending = await screen.getByText("Mes notes de frais");
+        expect(contentPending).toBeTruthy();
+      });
+      describe("When an error occurs on API", () => {
+        beforeEach(() => {
+          jest.spyOn(mockStore, "bills");
+          Object.defineProperty(window, "localStorage", {
+            value: localStorageMock,
+          });
+          window.localStorage.setItem(
+            "user",
+            JSON.stringify({
+              type: "Employee",
+              email: "a@a",
+            })
+          );
+          const root = document.createElement("div");
+          root.setAttribute("id", "root");
+          document.body.appendChild(root);
+          router();
+        });
+        test("fetches bills from an API and fails with 404 message error", async () => {
+          mockStore.bills.mockImplementationOnce(() => {
+            return {
+              list: () => {
+                return Promise.reject(new Error("Erreur 404"));
+              },
+            };
+          });
+          window.onNavigate(ROUTES_PATH.Bills);
+          await new Promise(process.nextTick);
+          const message = await screen.getByText(/Erreur 404/);
+          expect(message).toBeTruthy();
+        });
+
+        test("fetches messages from an API and fails with 500 message error", async () => {
+          mockStore.bills.mockImplementationOnce(() => {
+            return {
+              list: () => {
+                return Promise.reject(new Error("Erreur 500"));
+              },
+            };
+          });
+
+          window.onNavigate(ROUTES_PATH.Bills);
+          await new Promise(process.nextTick);
+          const message = await screen.getByText(/Erreur 500/);
+          expect(message).toBeTruthy();
+        });
+      });
     });
   });
 });
